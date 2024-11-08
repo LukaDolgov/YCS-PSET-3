9765c4b7bf8bba198b2f34ac142a6ce71a14ad7a
LukaDolgov <luka.dolgov@gmail.com>
2024-11-08 11:26:22 -0500

    nice changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import CustomButton from './CustomButton';
+import CustomCard from './CustomCard';
+import Box from './Box'
 
 function App() {
   return (
     <>
       <h1>Custom Components Practice</h1>
       <CustomButton text="Click Me" bgcolor="cornflowerblue" alertMessage="you clicked me!" />
+      <CustomCard title="My Card" description="this is my addition" extra_description="ooh, fancy" />
+      <Box initialColor="lightcoral" hoverColor="red" width="150px" height="150px" />
       {/* Add your custom components here */}
     </>
   );
